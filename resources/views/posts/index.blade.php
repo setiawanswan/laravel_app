6ac915fd9d8cee6dc775552392967d50bc901067
setiawan <setiawan.swn@work.brins.co.id>
2021-12-30 10:13:58 +0700

    partials component

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,11 +4,7 @@
 
 @section('content')
     @forelse($posts as $key => $post)
-    @if($loop->even)
-        <div> {{$key}}.{{ $post['title'] }} </div>
-    @else
-        <div style="background-color:silver"> {{$key}}.{{ $post['title'] }} </div>
-    @endif
+        @include('posts.partials.post')
     @empty
     No found Post !
     @endforelse
